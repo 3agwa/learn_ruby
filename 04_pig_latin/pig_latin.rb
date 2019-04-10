e77515e4555f55d379a774b56734a811fc6c3fe8
3agwa <a.mounir.112@gmail.com>
2019-04-10 18:16:26 +0200

    Added solution for pig latin problem

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,42 @@
 #write your code here
+#quiet
+#ietquay
+def get word
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+	if (word.include?("qu"))
+		word = word.sub("qu", "~")
+	end
+	if (vowels.include?(word[0]))
+		
+	elsif ((not vowels.include?(word[1])) and (not vowels.include?(word[0])) and (not vowels.include?(word[2])))
+		word = word + word[0]
+		word = word[1..word.length-1]
+		word = word + word[0]
+		word = word[1..word.length-1]
+		word = word + word[0]
+		word = word[1..word.length-1]		
+		
+	elsif ((not vowels.include?(word[1])) and (not vowels.include?(word[0])))
+		word = word + word[0]
+		word = word[1..word.length-1]
+		word = word + word[0]
+		word = word[1..word.length-1]		
+		
+	elsif(not vowels.include?(word[0]))
+		word = word + word[0]
+		word = word[1..word.length-1]
+		
+	end
+	word = word.sub("~", "qu")
+	return word + "ay"
+end
+
+def translate(word)
+	arr = word.split(" ")
+	if (arr.length == 1)
+		return word = get(word)
+
+	end
+	word.split(" ").map {|match| match = get(match)}.join(" ")
+end
